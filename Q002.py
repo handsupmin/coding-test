63fc05df2c5a176a1a9c08dca0da3b8ba6adcf0c
handsupmin <72061607+handsupmin@users.noreply.github.com>
2021-03-08 21:47:50 +0900

    Q002 complete

--- Q002.py
+++ Q002.py
@@ -16,8 +16,14 @@ second = data[n-2] # 두 번째로 큰 수
 result = 0
 
 while True:
-    for i in range(k):
-        if(m == 0):
+    for i in range(k): # 가장 큰 수를 K번 더하기
+        if(m == 0): # m이 0이면 반복문 탈출
             break;
         result += first
-    result += second
+        m -= 1 # 더할 때마다 1씩 빼기
+    if(m == 0): # m이 0이라면 반복문 탈출
+        break;
+    result += second # 두 번때로 큰 수를 한 번 더하기
+    m -= 1
+
+print(result) # 최종 결과 출력
