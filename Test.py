0632b3619bc5123c869c8b0be633d1c643e3d60e
handsupmin <72061607+handsupmin@users.noreply.github.com>
2021-03-28 17:44:55 +0900

    keep studying

--- Test.py
+++ Test.py
@@ -1,23 +1,14 @@
 # Test.py
 # 실험용 파일입니다.
 
-import time
-start = time.time()  # 시작 시간 저장
-array = [7, 8, 5, 9, 0, 3, 1, 6, 2, 4, 8, 8]
-result = []
+def minmaxSclaer(list):
+    print(list)
+    data = [[x] for x in list]
+    print(data)
+    data = [x[0] for x in data]
+    print(data)
+    return data
 
-result.append(array[0])
-index = 0
+list1 = [1,2,3,4,5,6,7,8]
+list1 = minmaxSclaer(list1)
 
-for i in range(1, len(array)):
-    for j in range(0, i):
-        if array[i] <= result[0]:
-            index = 0
-        elif array[i] > result[len(result)-1]:
-            index = len(result)
-        elif result[j] <= array[i] < result[j+1]:
-            index = j+1
-    result.insert(index, array[i])
-
-print(result)
-print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
