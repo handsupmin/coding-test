e65888189916101e4b066f057a59c4eff6aced4c
handsupmin <thstkdals0318@gmail.com>
2021-08-04 21:06:49 +0900

    keep studying

--- Concepts/Test.py
+++ Concepts/Test.py
@@ -1,43 +1,16 @@
 # Test.py
 # 실험용 파일입니다.
 
-def solution(arr):
-    answer = [0, 0]
-    n = len(arr)
-    half = n // 2
-    result = check(arr)
-    if sum(result) != 0:
-        answer[0] += result[0]
-        answer[1] += result[1]
-        return answer
-    else:
-        arr1 = [[-1] * half for _ in range(half)]
-        arr2 = [[-1] * half for _ in range(half)]
-        arr3 = [[-1] * half for _ in range(half)]
-        arr4 = [[-1] * half for _ in range(half)]
-        for i in range(n):
-            for j in range(n):
-                if i < half and j < half:
-                    arr1[i][j] = arr[i][j]
-                if i < half and j >= half:
-                    arr2[i][j-half] = arr[i][j]
-                if i >= half and j < half:
-                    arr3[i-half][j] = arr[i][j]
-                if i >= half and j >= half:
-                    arr4[i-half][j-half] = arr[i][j]
-        return [solution(arr1)[0] + solution(arr2)[0] + solution(arr3)[0] + solution(arr4)[0], solution(arr1)[1] + solution(arr2)[1] + solution(arr3)[1] + solution(arr4)[1]]
-def check(arr):
-    if count_arr(arr, 0) == len(arr) * len(arr):
-        return [count_arr(arr, 0), 0]
-    elif count_arr(arr, 1) == len(arr) * len(arr):
-        return [0, count_arr(arr, 1)]
-    else:
-        return [0, 0]
-    
-def count_arr(arr, num):
+def solution(n):
     count = 0
-    for i in range(len(arr)):
-        for j in range(len(arr)):
-            if arr[i][j] == num:
+    for i in range(1, (n//2 + 1)):
+        sum_of_num = 0
+        for j in range(i, n+1):
+            sum_of_num += j
+            if sum_of_num == n:
                 count += 1
-    return count
+            elif sum_of_num > n:
+                break
+    return count+1
+
+print(solution(15))
