dd79750c35931333347a1a1f1a9d45237d3c0fa6
handsupmin <thstkdals0318@gmail.com>
2021-10-05 18:06:35 +0900

    keep studying

--- Problems/Q110.py
+++ Problems/Q110.py
@@ -1,2 +1,20 @@
 # Q110.py
 # https://www.acmicpc.net/problem/2920
+
+music = list(map(int, input().split()))
+
+asc = 0
+de = 0
+
+for i in range(8):
+    if music[i] == i + 1:
+        asc += 1
+    elif music[i] == 8 - i:
+        de += 1
+
+if asc == 8:
+    print("ascending")
+elif de == 8:
+    print("descending")
+else:
+    print("mixed")
