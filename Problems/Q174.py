1d16e6c029c63f9d6a4285b61b739ba5ada8f98b
handsupmin <thstkdals0318@gmail.com>
2021-10-20 19:39:08 +0900

    keep studying

---
+++ Problems/Q174.py
@@ -0,0 +1,16 @@
+# Q174.py
+# https://www.acmicpc.net/problem/1927
+
+import heapq
+import sys
+
+input = sys.stdin.readline
+queue = []
+for _ in range(int(input())):
+    num = int(input())
+    if num == 0 and len(queue) == 0:
+        print(0)
+    elif num == 0:
+        print(-heapq.heappop(queue))
+    else:
+        heapq.heappush(queue, -num)
