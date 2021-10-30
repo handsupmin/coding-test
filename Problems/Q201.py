2ae66583a6af30b08538b1ea742c7b40414b1ef2
handsupmin <thstkdals0318@gmail.com>
2021-10-30 15:33:39 +0900

    keep studying

--- Problems/Q201.py
+++ Problems/Q201.py
@@ -1,2 +1,23 @@
 # Q201.py
-# 
+# https://www.acmicpc.net/problem/9461
+
+for _ in range(int(input())):
+    n = int(input())
+
+    d = [0] * n
+    
+    if n <= 3:
+        print(1)
+    elif n <= 4:
+        print(2)
+    else:
+        d[0] = 1
+        d[1] = 1
+        d[2] = 1
+        d[3] = 2
+        d[4] = 2
+
+        for i in range(5, n):
+            d[i] = d[i-1] + d[i-5]
+        
+        print(d[n-1])
