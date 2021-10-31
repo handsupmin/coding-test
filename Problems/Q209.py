77a2562001af9db326bc077d0d139b8992569b1d
handsupmin <thstkdals0318@gmail.com>
2021-10-31 21:46:15 +0900

    keep studying

--- Problems/Q209.py
+++ Problems/Q209.py
@@ -4,13 +4,15 @@
 n = int(input())
 d = [0] * (n + 1)
 
-d[1] = 1
-d[2] = 3
+if n == 1:
+    print(1)
 
-if 1 <= n <= 2:
-    print(d[n])
+elif n == 2:
+    print(3)
 
 else:
+    d[1] = 1
+    d[2] = 3
     for i in range(3, n + 1):
         d[i] = d[i - 1] + (d[i - 2] * 2)
     print(d[n] % 10007)
