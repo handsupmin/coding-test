b148777d746066c154550e4666d6f48895afb465
handsupmin <thstkdals0318@gmail.com>
2021-11-05 22:41:48 +0900

    keep studying

---
+++ Problems/Q214.cs
@@ -0,0 +1,14 @@
+// Q214.cs
+// https://programmers.co.kr/learn/courses/30/lessons/12954?language=csharp
+
+public class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        
+        for(long i = 0; i < n; i++) {
+            answer[i] = x * (i + 1);
+        }
+        
+        return answer;
+    }
+}
