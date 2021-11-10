7e92876af7e97c74123a53734eeee7ebab3dd398
handsupmin <thstkdals0318@gmail.com>
2021-11-10 19:25:37 +0900

    keep studying

--- Concepts/Note.cs
+++ Concepts/Note.cs
@@ -46,3 +46,24 @@ ListT[0].Count
 // String
 string answer = "";
 answer.Length // 문자열의 길이
+
+// Convert.ToInt32 vs Int32.Parse vs Int32.TryParse
+// 테스트를 위한 string 변수 생성 및 다양한 값 할당
+string a = "123";
+string b = "0.1";
+string c = null;
+// Convert.ToInt32
+Convert.ToInt32(a); // 123
+Convert.ToInt32(b); // FormatException
+Convert.ToInt32(c); // 0
+// Int32.Parse
+Int32.Parse(a); // 123
+Int32.Parse(b); // FormatException
+Int32.Parse(c); // ArgumentNullException
+// Int32.TryParse
+int i;
+Int32.TryParse(a, out i); // true (123)
+Int32.TryParse(b, out i); // false (0)
+Int32.TryParse(c, out i); // false (0)
+// Convert 와 Parse의 차이는 파라미터가 null 일때 0을 반환하는지, 예외처리를 해주는지의 차이이다.
+// 상황에 맞게 사용해야 하지만 가급적이면 안전하게 사용하기 위해서 자체 예외 핸들링을 해주는 TryParse 를 쓰도록 하자.
---
+++ Problems/Q218.cs
@@ -0,0 +1,24 @@
+// Q218.cs
+// https://programmers.co.kr/learn/courses/30/lessons/12947?language=csharp
+
+using System;
+
+public class Solution {
+    public bool solution(int x) {
+        bool answer = true;
+        string num_to_string = Convert.ToString(x);
+        int sum = 0;        
+        
+        foreach(char char_num in num_to_string)
+        {
+            sum += Convert.ToInt32(Convert.ToString(char_num));
+        }
+        
+        if (x % sum == 0)
+            answer = true;
+        else
+            answer = false;
+        
+        return answer;
+    }
+}
