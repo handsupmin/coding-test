a7d9446f164d6db54c3baf8a462b42d137e4889d
handsupmin <thstkdals0318@gmail.com>
2021-11-15 19:08:08 +0900

    keep studying

---
+++ Problems/Q224.cs
@@ -0,0 +1,20 @@
+// Q224.cs
+// https://programmers.co.kr/learn/courses/30/lessons/12934?language=csharp
+
+using System;
+
+public class Solution
+{
+    public long solution(long n)
+    {
+        double num = Math.Sqrt(n);
+        long integer = (int)num;
+        
+        if (integer == num)
+        {
+            integer ++;
+            return integer * integer;
+        }
+        return -1;
+    }
+}
