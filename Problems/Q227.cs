ee0060cddaea467b3b97e40098c00be3eb290e29
handsupmin <thstkdals0318@gmail.com>
2021-11-18 21:27:35 +0900

    keep studying

--- Problems/Q227.cs
+++ Problems/Q227.cs
@@ -1,3 +1,19 @@
 // Q227.cs
-// 
+// https://programmers.co.kr/learn/courses/30/lessons/12932
 
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] solution(long n)
+    {
+        var data = from c in n.ToString() select int.Parse(c.ToString());
+        List<int> numList = data.ToList();
+        numList.Reverse();
+        int[] numArray = numList.ToArray();
+        
+        return numArray;
+    }
+}
